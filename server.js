02e2b861ccc82c0bf900a62bf5ded3415c88cf26
deepakkumar9470 <dk397787@gmail.com>
2023-01-17 09:27:18 +0530

    mern-travel-2.0

--- server.js
+++ server.js
@@ -17,14 +17,25 @@ app.use('/api/users', userRoute);
 app.use('/api/pins', pinRoute);
 
 
+// Set static folder
 if (process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static('client/build'));
+app.use(express.static(path.join(__dirname, './client/build')));
   
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './client/build/index.html'),(err)=>{
+        res.status(500).send(err)
     });
-  }
+});
+}
+
+// if (process.env.NODE_ENV === 'production') {
+//     // Set static folder
+//     app.use(express.static('./client/build'));
+  
+//     app.get('*', (req, res) => {
+//       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//     });
+//   }
 
 // Connetion to db
 
